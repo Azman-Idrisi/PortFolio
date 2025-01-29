cca281e0ddcf8d56a3de8661ab2036a3a26a40b5
Azman <idrisiazman@gmail.com>
2025-01-29 22:09:48 +0530

    Bugs fixed

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
-        <Experience />
+        <Experience />  
       </div>
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,8 +6,6 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import MagicButton from "./ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa";
 
-
-
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
@@ -29,7 +27,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+            Dynamic Web Magic with React.js
           </p>
 
           <TextGenerateEffect
@@ -37,7 +35,7 @@ const Hero = () => {
             words="Transforming Concepts into seamless User Experiences"
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi, I&apos;m Azman, a Next.js Developer based in India.
+            Hi, I&apos;m Azman, a React.js Developer based in India.
           </p>
           <a href="#about">
             <MagicButton
